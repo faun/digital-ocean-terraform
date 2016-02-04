c8f47341faa8e10ad60c4b521d3a3a2b760d51c5
Faun <github@faun.me>
2016-02-04 15:07:44 -0800

    Move dnsimple to separate file

--- digital-ocean.tf
+++ digital-ocean.tf
@@ -1,21 +1,3 @@
-variable "dnsimple_subdomain" {}
-variable "dnsimple_domain" {}
-variable "dnsimple_email" {}
-variable "dnsimple_token" {}
-
-provider "dnsimple" {
-  email = "${var.dnsimple_email}"
-  token = "${var.dnsimple_token}"
-}
-
-resource "dnsimple_record" "web" {
-  domain = "${var.dnsimple_domain}"
-  name   = "${var.dnsimple_subdomain}"
-  value  = "${digitalocean_droplet.web.ipv4_address}"
-  type   = "A"
-  ttl    = 30
-}
-
 variable "digitalocean_token" {}
 
 provider "digitalocean" {
